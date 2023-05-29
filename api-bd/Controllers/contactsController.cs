18b122a30466bf984e8b85542dfa7df4e779cedf
Artur-Lambo <lamboartur94@gmail.com>
2023-05-29 16:35:58 +0200

    joindre la base de données

--- api-bd/Controllers/contactsController.cs
+++ api-bd/Controllers/contactsController.cs
@@ -1,49 +1,74 @@
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Net.Http;
 using System.Net.Http.Headers;
 using System.Threading.Tasks;
 
-namespace api_bd.Controllers;
 
-[ApiController]
-[Route("[controller]")]
-
-
-public class contactsController : ControllerBase
+namespace api_bd.Controllers
 {
-    private readonly HttpClient _httpClient;
-
-    public contactsController(HttpClient httpClient)
+    [ApiController]
+    [Route("[controller]")]
+    public class contactsController : ControllerBase
     {
-        _httpClient = httpClient;
-    }
+        private readonly HttpClient _httpClient;
+        private readonly IConfiguration _configuration;
 
-    [HttpGet]
-    public async Task<IActionResult> Get()
-    {
-        try
+        public contactsController(HttpClient httpClient, IConfiguration configuration)
         {
-
-            _httpClient.BaseAddress = new Uri("http://your-api-url/"); //On recupere l'uri depuis appsettings
-            _httpClient.DefaultRequestHeaders.Accept.Clear();
-            _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-
-
-            HttpResponseMessage response = await _httpClient.GetAsync("api/contacts");
-            response.EnsureSuccessStatusCode();
-
-            // Read the response content
-            string responseBody = await response.Content.ReadAsStringAsync();
-
-            // Return the response
-            return Ok(responseBody);
+            _httpClient = httpClient;
+            _configuration = configuration;
         }
-        catch (Exception ex)
+
+        [HttpGet]
+        public async Task<IActionResult> Get()
         {
-            return StatusCode(500, ex.Message);
+            try
+            {
+                string bddUri = _configuration.GetValue<string>("BDDUri");
+
+                _httpClient.BaseAddress = new Uri(bddUri);
+                _httpClient.DefaultRequestHeaders.Accept.Clear();
+                _httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+                HttpResponseMessage response = await _httpClient.GetAsync("");
+                Console.WriteLine(response.StatusCode);
+                string responseBody = await response.Content.ReadAsStringAsync();
+
+                return Ok(responseBody);
+            }
+            catch (Exception ex)
+            {
+                return StatusCode(500, ex.Message);
+            }
         }
+
+        [HttpGet("tables")]
+        public IActionResult GetTables()
+        {
+            string username = _configuration.GetValue<string>("BDDAuthentication:Username");
+            string password = _configuration.GetValue<string>("BDDAuthentication:Password");
+            string connectionString = _configuration.GetConnectionString("DefaultConnection");
+            string connectionStringWithAuth = $"{connectionString};User Id={username};Password={password}";
+
+            using (var connection = new SqlConnection(connectionStringWithAuth))
+            {
+                connection.Open();
+
+                DataTable tables = connection.GetSchema("Tables");
+
+                var tableNames = tables.Rows.Cast<DataRow>().Select(row => row["TABLE_NAME"].ToString());
+
+                return Ok(tableNames);
+            }
+        }
+
+        // ...
+
+
     }
-  
 }
-
--- api-bd/Program.cs
+++ api-bd/Program.cs
@@ -1,11 +1,19 @@
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var configuration = new ConfigurationBuilder()
+    .SetBasePath(Directory.GetCurrentDirectory())
+    .AddJsonFile("appsettings.json")
+    .Build();
+
+builder.Services.AddSingleton(configuration);
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient();
+
+
 
 var app = builder.Build();
 
@@ -14,12 +22,13 @@ if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
+    
 }
 
-app.UseHttpsRedirection();
-
+app.UseRouting();
 app.UseAuthorization();
-
 app.MapControllers();
-
 app.Run();
+
+
+
